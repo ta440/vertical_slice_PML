57e9eada9a5646ee7d41f937212f66d0acf45037
Timothy Andrews <ta440@github.com>
2026-04-06 19:20:42 +0100

    add parameter sensitivity scripts

---
+++ acoustic_buoyancy/PML_acoustic_buoyancy_vary_beta.py
@@ -0,0 +1,149 @@
+'''
+A script to solve the comppressible Euler equations with the addition
+of topography. This is part of experiments to determine if I 
+can make a simple system that contains
+orographically-driven gravity waves.
+'''
+
+from firedrake import (
+    as_vector, VectorFunctionSpace, PeriodicIntervalMesh, ExtrudedMesh,
+    SpatialCoordinate, exp, pi, cos, Function, Mesh, Constant, conditional
+)
+from gusto import (
+    Domain, CompressibleParameters, logger, PMLParameters,
+    OutputParameters, IO, SSPRK3, DGUpwind, SemiImplicitQuasiNewton,
+    compressible_hydrostatic_balance, SpongeLayerParameters, Exner, ZComponent,
+    Perturbation, SUPGOptions, TrapeziumRule, MaxKernel, MinKernel,
+    CompressibleEulerEquations, SubcyclingOptions, RungeKuttaFormulation,
+    Timestepper, RK4, XComponent,ForwardEuler, BoussinesqEquations, BoussinesqParameters,
+    boussinesq_hydrostatic_balance, LinearAcousticBuoyancyEquations,
+    PMLParameters, KineticEnergy, VerticalKineticEnergy, time_derivative, transport
+)
+
+#######################################################################
+
+domain_width = 100.e3    # width of domain in x direction, in m
+domain_height = 20.e3    # height of model top, in m
+
+# Set dx = dx = 500 m
+ncolumns=200
+nlayers=40
+
+# Time parameters
+dt=0.25
+tmax=100.0
+dumpfreq=4
+
+# Pressure perturbation
+pert = 10              # Amplitude of perturbation
+d = 1.e3                 # Gaussian half-width for the pressure perturbation
+xc = 0.5*domain_width   # x location of the perturbation
+zc = 0.5*domain_height  # z location of the perturbation
+
+# Other parameters
+cs = 350                 # Speed of sound, m/s
+
+# PML parameters. 
+# Vary beta, the PML tolerance
+tol = 1e-5
+gamma0 = 0.25
+
+savename = f'PML_acoustic_buoyancy_beta_{tol}_gamma0_{gamma0}'
+
+# ------------------------------------------------------------------------ #
+# Our settings for this set up
+# ------------------------------------------------------------------------ #
+
+spinup_steps = 5  # Not necessary but helps balance initial conditions
+alpha = 0.51      # Necessary to absorb grid scale waves
+element_order = 1
+
+base_mesh = PeriodicIntervalMesh(ncolumns, domain_width)
+mesh = ExtrudedMesh(
+    base_mesh, layers=nlayers, layer_height=domain_height/nlayers
+)
+Vc = VectorFunctionSpace(mesh, "DG", 2)
+
+domain = Domain(mesh, dt, "CG", element_order)
+
+# Equation
+parameters = BoussinesqParameters(mesh, cs=cs)
+PML_pars = PMLParameters(mesh, H=domain_height, gamma0=gamma0, tol=tol)
+
+eqns = LinearAcousticBuoyancyEquations(
+    domain=domain, parameters=parameters, PML_options=PML_pars
+)
+
+# I/O
+output = OutputParameters(
+        dirname=savename, 
+        dumpfreq=dumpfreq, 
+        dump_vtus=False, 
+        dump_nc=True,
+    )
+
+
+diagnostic_fields = [Perturbation('b'), ZComponent('u'), XComponent('u'), ZComponent('q_u'), XComponent('q_u'), 
+                     KineticEnergy(), VerticalKineticEnergy()]
+
+io = IO(domain, output, diagnostic_fields=diagnostic_fields)
+
+suboptions = {}
+suboptions.update({'b': [time_derivative, transport]})
+b_opts = SUPGOptions(suboptions=suboptions)
+
+stepper = Timestepper(
+    eqns, RK4(domain, options=b_opts), io
+)
+
+# ------------------------------------------------------------------------ #
+# Initial conditions
+# ------------------------------------------------------------------------ #
+
+u0 = stepper.fields("u")
+p0 = stepper.fields("p")
+b0 = stepper.fields("b")
+q_u0 = stepper.fields("q_u")
+q_p0 = stepper.fields("q_p")
+
+# spaces
+Vb = b0.function_space()
+Vp = p0.function_space()
+
+x, z = SpatialCoordinate(mesh)
+
+# Define a Gaussian pertubration
+expr = conditional((x > 0.3*domain_width),
+                     conditional(x < 0.7*domain_width,
+                                 conditional(z > 0.3*domain_height,
+                                             conditional(z < 0.7*domain_height,
+                                                         pert*exp(-((x-xc)**2 + (z-zc)**2)/d**2),
+                                                         0), 0), 0), 0)
+
+# Apply the perturbation to the buoyancy
+#b_b = Function(Vb).interpolate(b_expr)
+#b0.interpolate(b_b)
+
+#p_b = Function(Vp)
+#boussinesq_hydrostatic_balance(eqns, b_b, p_b)
+#p0.assign(p_b)
+
+p_b = Function(Vp).interpolate(expr)
+b0.assign(Constant(0.0))
+p0.assign(p_b)
+b_b = Function(Vb).interpolate(b0)
+
+u0.project(as_vector([Constant(0.0), Constant(0.0)]))
+
+# PML variables all set to zero
+q_u0.project(as_vector([Constant(0.0), Constant(0.0)]))
+q_p0.assign(Constant(0.0))
+
+# set the background buoyancy
+stepper.set_reference_profiles([('p', p_b), ('b', b_b)])
+
+# ------------------------------------------------------------------------ #
+# Run
+# ------------------------------------------------------------------------ #
+
+stepper.run(t=0, tmax=tmax)
---
+++ acoustic_buoyancy/compare_PML_errors_diff_A.py
@@ -0,0 +1,162 @@
+'''
+Compare errors with different 
+values of gamma0 for the PML.
+
+'''
+import numpy as np
+from matplotlib import pyplot as plt
+from netCDF4 import Dataset
+import matplotlib
+import matplotlib.colors as colors
+import cartopy.crs as ccrs
+import os
+from os.path import abspath, dirname
+from tomplot import (
+    set_tomplot_style, tomplot_cmap, plot_contoured_field,
+    add_colorbar_ax, tomplot_field_title, tomplot_contours,
+    extract_gusto_coords, extract_gusto_field, reshape_gusto_data
+)
+from firedrake import errornorm
+####################################
+# Reference solution:
+ref_sol = 'acoustic_buoyancy_ref'
+
+gamma = 0.25
+
+A1 = 0.5
+A2 = 1
+A3 = 2
+A4 = 3
+A5 = 4
+
+
+# Different gamma solutions
+PML_sol1 = f'PML_acoustic_buoyancy_A_{A1}_gamma0_{gamma}'
+PML_sol2 = f'PML_acoustic_buoyancy_A_{A2}_gamma0_{gamma}'
+PML_sol3 = f'PML_acoustic_buoyancy_gamma0_{gamma}'
+PML_sol4 = f'PML_acoustic_buoyancy_A_{A4}_gamma0_{gamma}'
+PML_sol5 = f'PML_acoustic_buoyancy_A_{A5}_gamma0_{gamma}'
+
+# Undamped error for reference
+undamped_sol = 'acoustic_buoyancy_no_damp'
+
+# Field to compute the error in
+field_name = 'u_z'
+
+# Maximum height to compute errors
+z_max = 18
+zlims = [0,z_max]
+
+figure_stem = f'{abspath(dirname(__file__))}/figures/'
+savename = f'{figure_stem}acoustic_buoyancy_compare_PML_errors_diff_A_{field_name}'
+
+ref_results = f'{abspath(dirname(__file__))}/results/{ref_sol}/field_output.nc'
+PML_results1 = f'{abspath(dirname(__file__))}/results/{PML_sol1}/field_output.nc'
+PML_results2 = f'{abspath(dirname(__file__))}/results/{PML_sol2}/field_output.nc'
+PML_results3 = f'{abspath(dirname(__file__))}/results/{PML_sol3}/field_output.nc'
+PML_results4 = f'{abspath(dirname(__file__))}/results/{PML_sol4}/field_output.nc'
+PML_results5 = f'{abspath(dirname(__file__))}/results/{PML_sol5}/field_output.nc'
+undamped_results = f'{abspath(dirname(__file__))}/results/{undamped_sol}/field_output.nc'
+
+ref_data = Dataset(ref_results, 'r')
+PML_data1 = Dataset(PML_results1, 'r')
+PML_data2 = Dataset(PML_results2, 'r')
+PML_data3 = Dataset(PML_results3, 'r')
+PML_data4 = Dataset(PML_results4, 'r')
+PML_data5 = Dataset(PML_results5, 'r')
+undamped_data = Dataset(undamped_results, 'r')
+
+# Arrays to store errors
+PML_errs1 = []
+PML_errs2 = []
+PML_errs3 = []
+PML_errs4 = []
+PML_errs5 = []
+undamped_errs = []
+
+# Time values:
+time = ref_data['time']
+
+for i in np.arange(len(time)):
+    t = time[i]
+
+    ref_field = extract_gusto_field(ref_data, field_name, time_idx=i)
+    PML_field1 = extract_gusto_field(PML_data1, field_name, time_idx=i)
+    PML_field2 = extract_gusto_field(PML_data2, field_name, time_idx=i)
+    PML_field3 = extract_gusto_field(PML_data3, field_name, time_idx=i)
+    PML_field4 = extract_gusto_field(PML_data4, field_name, time_idx=i)
+    PML_field5 = extract_gusto_field(PML_data5, field_name, time_idx=i)
+    undamped_field = extract_gusto_field(undamped_data, field_name, time_idx=i)
+
+    # Crop fields to be outside of the PML
+    ref_coords_X, ref_coords_Z = extract_gusto_coords(ref_data, field_name)
+    ref_inds = np.where(ref_coords_Z <= z_max)
+    ref_field = ref_field[ref_inds]
+
+    test_coords_X, test_coords_Z = extract_gusto_coords(PML_data1, field_name)
+    test_inds = np.where(test_coords_Z <= z_max)
+    PML_field1 = PML_field1[test_inds]
+    PML_field2 = PML_field2[test_inds]
+    PML_field3 = PML_field3[test_inds]
+    PML_field4 = PML_field4[test_inds]
+    PML_field5 = PML_field5[test_inds]
+    undamped_field = undamped_field[test_inds]
+
+    # Normalised L1 error
+    #no_damp_error = np.sum(np.abs(ref_field - no_damp_field))/np.sum(np.abs(ref_field))
+    #sponge_error = np.sum(np.abs(ref_field - sponge_field))/np.sum(np.abs(ref_field))
+    #PML_error = np.sum(np.abs(ref_field - PML_field))/np.sum(np.abs(ref_field))
+
+    # What about L2?
+    PML_error1 = np.sqrt(np.sum((ref_field - PML_field1)**2))/np.sum(np.abs(ref_field))
+    PML_error2 = np.sqrt(np.sum((ref_field - PML_field2)**2))/np.sum(np.abs(ref_field))
+    PML_error3 = np.sqrt(np.sum((ref_field - PML_field3)**2))/np.sum(np.abs(ref_field))
+    PML_error4 = np.sqrt(np.sum((ref_field - PML_field4)**2))/np.sum(np.abs(ref_field))
+    PML_error5 = np.sqrt(np.sum((ref_field - PML_field5)**2))/np.sum(np.abs(ref_field))
+    undamped_error = np.sqrt(np.sum((ref_field - undamped_field)**2))/np.sum(np.abs(ref_field))
+
+    PML_errs1.append(PML_error1)
+    PML_errs2.append(PML_error2)
+    PML_errs3.append(PML_error3)
+    PML_errs4.append(PML_error4)
+    PML_errs5.append(PML_error5)
+    undamped_errs.append(undamped_error)
+
+PML_errs1 = np.asarray(PML_errs1)
+PML_errs2 = np.asarray(PML_errs2)
+PML_errs3 = np.asarray(PML_errs3)
+PML_errs4 = np.asarray(PML_errs4)
+PML_errs5 = np.asarray(PML_errs5)
+undamped_errs = np.asarray(undamped_errs)
+
+print(f'Final PML 1 error is {PML_errs1[-1]}')
+print(f'Final PML 2 error is {PML_errs2[-1]}')
+print(f'Final PML 3 error is {PML_errs3[-1]}')
+print(f'Final PML 4 error is {PML_errs4[-1]}')
+print(f'Final PML 5 error is {PML_errs5[-1]}')
+print(f'Final undamped error is {undamped_errs[-1]}')
+
+t_min = 20
+
+plt.figure()
+plt.semilogy(time[t_min:], PML_errs1[t_min:], label=r'$A$ = ' f'{A1}')
+plt.semilogy(time[t_min:], PML_errs2[t_min:], label=r'$A$ = ' f'{A2}')
+plt.semilogy(time[t_min:], PML_errs3[t_min:], label=r'$A$ = ' f'{A3}')
+plt.semilogy(time[t_min:], PML_errs4[t_min:], label=r'$A$ = ' f'{A4}')
+plt.semilogy(time[t_min:], PML_errs5[t_min:], label=r'$A$ = ' f'{A5}')
+# Additionally plot the undamped error?
+plt.semilogy(time[t_min:], undamped_errs[t_min:], label='No damping')
+
+plt.legend(loc='center right', prop={'size': 10}, bbox_to_anchor=(1.3, 0.5))
+plt.xlabel('Time (s)', size=14)
+plt.ylabel('Error in the lower domain', size=14)
+plt.xlim([t_min,time[-1]])
+plt.ylim([2e-6,1e-1])
+plt.xticks([25,50,75,100])
+plt.grid()
+
+
+plt.savefig(savename, bbox_inches='tight')
+print(f'saved figure to {savename}')
+
+plt.close()
